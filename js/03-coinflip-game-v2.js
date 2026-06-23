de4ea321d936291e050d47681aba9e9acb8309de
aboomar13 <aboomar5464@gmail.com>
2026-06-23 03:12:19 -0700

    coinflip game v2 created

---
+++ js/03-coinflip-game-v2.js
@@ -0,0 +1,11 @@
+// let coinFlip;
+// let flips = parseInt(prompt('How many times should the coin flip?'));
+
+// for (let i = 1; i <= flips; i++) {
+//     let coinFlip = Math.round(Math.random())
+//     if (!coinFlip) {
+//         console.log('Heads')
+//     } else {
+//         console.log('Tails')
+//     }
+// }
