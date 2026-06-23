bcec7999edcc90c2197baa9c41c8dcaa265bdacf
aboomar13 <aboomar5464@gmail.com>
2026-06-23 03:11:17 -0700

    larger smaller js created

---
+++ js/01-larger-or-smaller.js
@@ -0,0 +1,11 @@
+// let num01 = parseInt(prompt('Enter the first number!'))
+// let num02 = parseInt(prompt('Enter the secont number!'))
+// if (num01 > num02) {
+//     document.write(`the larger number is: ${num01}`)
+// }
+// else if (num02 > num01) {
+//     document.write(`the larger number is: ${num02}`)
+// }
+// else {
+//     document.write('Both numbers are equal.')
+// }
